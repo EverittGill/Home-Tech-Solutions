9b56aa7335126319b29e91002a498d1e8fc69d05
EverittGill <eg.everittgill@gmail.com>
2023-09-26 10:51:37 -0400

    standing by commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Please Stand By</p>
+        <p>Standing By</p>
         <a
           className="App-link"
           href="https://reactjs.org"
